669cdca8dbd9aed245215161cf531cbb1bfdb7c0
hidden0x001 <tictoc0x001@gmail.com>
2020-03-13 10:18:46 +0100

    fix0.1

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
-db.on('error', err =>{console.error(err)});
+db.on('error', err =>{throw err;});
 db.once('open', ()=>{console.log('Connected to Mongoose')});
 
 app.post('/', urlencodedParser,(req, res)=>{
